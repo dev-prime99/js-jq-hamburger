b0ccddf4e5302b17a90d9028556e155d8526432f
dev-prime99 <prime99@icloud.com>
2019-11-05 15:32:36 +0100

    animation added

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 
+$(".hamburger-menu").css("opacity", "0");
+
 $('.header-right > a').click(function(){
   $(".hamburger-menu").css("display", "block");
+  $(".hamburger-menu").animate({opacity: "1"}, "1500");
 });
 
 $(".close").click(function(){
+  $(".hamburger-menu").animate({opacity: "0"}, "slow");
   $(".hamburger-menu").css("display", "none");
 });
