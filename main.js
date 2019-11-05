368c5c091c05dc1bb10ba375c04f1df2d12c6a9f
dev-prime99 <prime99@icloud.com>
2019-11-05 15:18:37 +0100

    ex completed

---
+++ main.js
@@ -0,0 +1,8 @@
+
+$('.header-right > a').click(function(){
+  $(".hamburger-menu").css("display", "block");
+});
+
+$(".close").click(function(){
+  $(".hamburger-menu").css("display", "none");
+});
